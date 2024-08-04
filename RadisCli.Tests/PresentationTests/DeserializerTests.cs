e9e5684abbc6e2df1f0fd614dccd1dfd630ad49b
Phat Tang <tlphat18@apcs.vn>
2024-08-04 10:34:58 +0700

    feat: implement deserializer

---
+++ RadisCli.Tests/PresentationTests/DeserializerTests.cs
@@ -0,0 +1,35 @@
+﻿using FluentAssertions;
+using RadisCli.Presentation;
+
+namespace RadisCli.Tests.PresentationTests;
+
+public class DeserializerTests
+{
+    [Fact]
+    public void Deserializer_GivenBulkStringResp_ReturnsString()
+    {
+        string actual = Deserializer.Deserialize("$5\r\nhello\r\n");
+        actual.Should().Be("hello");
+    }
+
+    [Fact]
+    public void Deserialize_GivenNullBulkStringResp_ReturnsNilString()
+    {
+        string actual = Deserializer.Deserialize("$-1\r\n");
+        actual.Should().Be("(nil)");
+    }
+
+    [Fact]
+    public void Deserialize_GivenSimpleStringResp_ReturnsString()
+    {
+        string actual = Deserializer.Deserialize("+OK\r\n");
+        actual.Should().Be("OK");
+    }
+
+    [Fact]
+    public void Deserialize_GivenNullResp_ReturnsNilString()
+    {
+        string actual = Deserializer.Deserialize("_\r\n");
+        actual.Should().Be("(nil)");
+    }
+}
---
+++ RadisCli/Presentation/Deserializer.cs
@@ -0,0 +1,32 @@
+﻿namespace RadisCli.Presentation;
+
+public class Deserializer
+{
+    public static string Deserialize(string data)
+    {
+        StringReader reader = new StringReader(data);
+        string header = ReadLineOrThrow(reader, new ArgumentException("Null header"));
+
+        // Parse null and null bulk string
+        if ("_".Equals(header) || "$-1".Equals(header))
+        {
+            return "(nil)";
+        }
+
+        // Parse simple string
+        if (header.StartsWith('+'))
+        {
+            return header[1..];
+        }
+
+        string body = ReadLineOrThrow(reader, new ArgumentException("Null body"));
+        reader.Close();
+
+        return body;
+    }
+
+    private static string ReadLineOrThrow(StringReader reader, Exception exception)
+    {
+        return reader.ReadLine() ?? throw exception;
+    }
+}
