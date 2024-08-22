eb1883ceb9e781d89882cc93dd1e506554099b2e
Phat Tang <tlphat18@apcs.vn>
2024-08-22 10:25:00 +0700

    test: migrate integration test to a separate project

---
+++ RadisCli.IntegrationTests/ConnectionTests/ClientTests.cs
@@ -0,0 +1,48 @@
+﻿using FluentAssertions;
+using RadisCli.Connection;
+namespace RadisCli.Tests.ConnectionTests;
+
+public class ClientTests
+{
+    [Fact]
+    public void Client_GivenPing_ReturnsPong()
+    {
+        var client = new Client("127.0.0.1", 6379);
+        client.Connect();
+
+        string request = "PING";
+        string actual = client.Send(request);
+
+        client.Close();
+
+        actual.Should().Be("PONG");
+    }
+
+    [Fact]
+    public void Client_GivenGetNonExistingKey_ReturnsNull()
+    {
+        Client client = new("127.0.0.1", 6379);
+        client.Connect();
+
+        string request = "GET x";
+        string actual = client.Send(request);
+
+        client.Close();
+
+        actual.Should().Be("(nil)");
+    }
+
+    [Fact]
+    public void Client_GivenGetNonExistingKeyWithRedundantSpace_ReturnsNull()
+    {
+        Client client = new("127.0.0.1", 6379);
+        client.Connect();
+
+        string request = "GET  x";
+        string actual = client.Send(request);
+
+        client.Close();
+
+        actual.Should().Be("(nil)");
+    }
+}
---
+++ RadisCli.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace RadisCli.IntegrationTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
